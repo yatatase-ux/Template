a3efedefdb7c707bc3772d0396bcec1182841236
4D-08\koyo <a>
2026-08-25 13:39:23 +0900

    ３つの関数をコメントアウトし、テンプレートを用いた同様の関数に置き換え

--- Template.cpp
+++ Template.cpp
@@ -1,15 +1,21 @@
 ﻿#include <iostream>
-int max_int(int a, int b)
-{
-	return a > b ? a : b;
-}
+//int max_int(int a, int b)
+//{
+//	return a > b ? a : b;
+//}
+//
+//long max_long(long a, long b)
+//{
+//	return a > b ? a : b;
+//}
+//
+//float max_float(float a, float b)
+//{
+//	return a > b ? a : b;
+//}
 
-long max_long(long a, long b)
-{
-	return a > b ? a : b;
-}
-
-float max_float(float a, float b)
+template<class T>
+T max(T a, T b)
 {
 	return a > b ? a : b;
 }
@@ -19,9 +25,13 @@ int main()
 	float a = 10.1;
 	float b = 10.2;
 
-	std::cout << "max: " << max_int(a, b) << std::endl;
-	std::cout << "max: " << max_long(a, b) << std::endl;
-	std::cout << "max: " << max_float(a, b) << std::endl;
+	//std::cout << "max: " << max_int(a, b) << std::endl;
+	//std::cout << "max: " << max_long(a, b) << std::endl;
+	//std::cout << "max: " << max_float(a, b) << std::endl;
+
+	std::cout << "max: " << max(a, b) << std::endl;
+	std::cout << "max: " << max(a, b) << std::endl;
+	std::cout << "max: " << max(a, b) << std::endl;
 
 } 
 
