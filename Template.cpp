a53493d65ceae6ed6373ffe4887a606e82f4dc8e
4D-08\koyo <a>
2026-08-25 13:34:11 +0900

    2数を比較して大きい方の値を返す関数を３種類追加

--- Template.cpp
+++ Template.cpp
@@ -1,13 +1,31 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
+int max_int(int a, int b)
+{
+	return a > b ? a : b;
+}
+
+long max_long(long a, long b)
+{
+	return a > b ? a : b;
+}
+
+float max_float(float a, float b)
+{
+	return a > b ? a : b;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
-}
+	float a = 10.1;
+	float b = 10.2;
+
+	std::cout << "max: " << max_int(a, b) << std::endl;
+	std::cout << "max: " << max_long(a, b) << std::endl;
+	std::cout << "max: " << max_float(a, b) << std::endl;
+
+} 
+
 
 /*
 ・テンプレート…型や定数をパラメータ化して、コンパイル時に実体化する仕組み
-
-
 */
