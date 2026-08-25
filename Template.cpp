61cbfe44f4e4179af8d7e2401ebdb6b921902295
4D-08\koyo <a>
2026-08-25 15:06:19 +0900

    演習①：配列型の判定を追加

--- Template.cpp
+++ Template.cpp
@@ -56,9 +56,24 @@ struct NoPointer : IsPointer<int> {
 
 };
 
+//-----------------------------------------------------------------------------------
+// 演習①：配列型を判定する(期待する結果：0, 1, 1)
+template<typename T> struct IsArray {
+	static constexpr bool value = false;
+};
+
+template<typename U, std::size_t N> struct IsArray<U[N]> {
+	static constexpr bool value = true;
+};
+
+//-----------------------------------------------------------------------------------
+// 演習②：const型を判定する(期待する結果：0, 1, 0, 1)
+
+
 //===================================================================================
 int main()
 {
+
 //	int a = 20;
 //	int b = 21;
 //	float c = 10.1f;
@@ -75,7 +90,7 @@ int main()
 //
 //	max_out(a, b);
 //	max_out(c, d);
-// 
+
 //-------------------------------------------------------------------------------
 
 	//StaticArray<int, 3> data;
@@ -92,11 +107,17 @@ int main()
 
 //------------------------------------------------------------------------------
 
-	std::cout << IsPointer<int>::value << std::endl;
-	std::cout << IsPointer<int*>::value << std::endl;
-	std::cout << IsPointer<float>::value << std::endl;
-	std::cout << IsPointer<float*>::value << std::endl;
-	std::cout << NoPointer::value << std::endl;
+	//std::cout << IsPointer<int>::value << std::endl;
+	//std::cout << IsPointer<int*>::value << std::endl;
+	//std::cout << IsPointer<float>::value << std::endl;
+	//std::cout << IsPointer<float*>::value << std::endl;
+	//std::cout << NoPointer::value << std::endl;
+
+//------------------------------------------------------------------------------
+
+	std::cout << IsArray<int>::value << std::endl;
+	std::cout << IsArray<int[10]>::value << std::endl;
+	std::cout << IsArray<float[5]>::value << std::endl;
 
 } 
 
